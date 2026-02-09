b2ea02a825129743cb20e8fc3700f4a7c45b1a32
aleyamayab <kingdom_ale@hotmail.com>
2026-02-09 08:50:15 -0600

    fix test

--- devsu-demo-devops-python/devsu-demo-devops-python/api/tests.py
+++ devsu-demo-devops-python/devsu-demo-devops-python/api/tests.py
@@ -5,23 +5,33 @@ from .models import User
 
 class TestUserView(APITestCase):
     def setUp(self):
-        user = User(name='Test1', dni='09876543210')
-        user.save()
+        self.user = User(name='Test1', dni='09876543210')
+        self.user.save()
         self.url = reverse("users-list")
         self.data = {'name': 'Test2', 'dni': '09876543211'}
 
     def test_post(self):
         response = self.client.post(self.url, self.data, format='json')
         self.assertEqual(response.status_code, 201)
-        self.assertEqual(json.loads(response.content), {"id": 2, "name":"Test2", "dni":"09876543211"})
+        payload = json.loads(response.content)
+        self.assertEqual(payload["name"], "Test2")
+        self.assertEqual(payload["dni"], "09876543211")
+        self.assertIsInstance(payload.get("id"), int)
         self.assertEqual(User.objects.count(), 2)
 
     def test_get_list(self):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(json.loads(response.content)), 1)
+        payload = json.loads(response.content)
+        self.assertEqual(len(payload), 1)
+        self.assertEqual(payload[0]["id"], self.user.id)
+        self.assertEqual(payload[0]["name"], "Test1")
+        self.assertEqual(payload[0]["dni"], "09876543210")
 
     def test_get(self):
-        response = self.client.get(self.url + '1/')
+        response = self.client.get(reverse("users-detail", args=[self.user.id]))
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(json.loads(response.content), {"id": 1, "name":"Test1", "dni":"09876543210"})
+        self.assertEqual(
+            json.loads(response.content),
+            {"id": self.user.id, "name": "Test1", "dni": "09876543210"}
+        )
